2f5e318def4c44ca0ab8262e6746207c6f506976
tamyt <tamitugues@gmail.com>
2018-06-27 01:48:19 -0300

    fixed json(?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser') ;
 const path=require(`path`);
 
-const fs = require(`fs`);
+const fs = require(`fs`);/**File system module */
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -13,14 +13,18 @@ const util = require(`util`);
 
 app.locals.allObjects= require(`./data.json`);
 
+
 app.use(express.json());
 app.use(expressValidator());
+
 app.use(express.static(path.join(__dirname, `public`)));
 app.use(`/bootstrap` ,express.static(__dirname+`/node_modules/bootstrap/dist/css/`));
 app.use(`/js` ,express.static(__dirname+`/public/js/pug.js`));
+/**express.static,funcion de middleware para el servicio de archivos estaticos*/
 
 app.set(`views`,path.join(__dirname,`views`));
 app.set(`view engine`,`pug`);
+
 
 
 app.get(`/ping`,(req, res) =>{
@@ -31,7 +35,7 @@ app.get(`/user/form`,(req, res) =>{
    
 });
 
-app.post(`/user`, urlencodedParser,(req, res, next)=>{
+app.post(`/user`, urlencodedParser,(req, res)=>{
 	
 	req.checkBody(`firstn`,`Invalid name`).isLength({max:30});
 	req.checkBody(`surname`,`Invalid surname`).isLength({max:30});
@@ -47,8 +51,7 @@ app.post(`/user`, urlencodedParser,(req, res, next)=>{
      }
      else{  
 
-     	const data= { 
-     		id:  allObjects.length + 1,
+     	const data= { 	
 		firstn:  req.body.firstn,
 	   surname:  req.body.surname,
 		mobile:  req.body.mobile,
@@ -57,21 +60,22 @@ app.post(`/user`, urlencodedParser,(req, res, next)=>{
 		 allObjects.push(data);
         const word = JSON.stringify(allObjects, null, 2);  
         fs.writeFile(`data.json`, word, (error)=>{
-
+         console.log("error");
     } );
       
 	res.redirect(`/user/list`);
 }
 
-  next();
+ 
   });
         
-
+/**app.method(path,handler) */
 app.get(`/user/list`,(req, res) =>{
 	
   res.render(`list`);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;/**Inicia el servidor,escuchando las conexiones en el puerto 3000
+o en el puerto que sea asignado */
 app.listen(port, () => console.log(`Listening on port ${port}...`));
  
